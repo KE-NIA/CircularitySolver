96eae910daedba828c99ecd638854f8596d9723e
KE-NIA <kania.kenneth@web.de>
2017-01-17 15:42:48 +0100

    Pull Request edited?! #1
    Martin likes my code : )

--- src/components/TrackerNavBar.js
+++ src/components/TrackerNavBar.js
@@ -13,23 +13,6 @@ export default class TrackerNavbar extends React.Component {
   render() {
       return(
         <div>
-          {/*<Navbar inverse>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="#/startscreen">Home</a>
-              </Navbar.Brand>
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav>
-                <li><a href="#/facecheck">FaceCheck</a></li>
-              </Nav>
-              <Nav pullRight>
-                <NavItem >
-                  <TrackerStatus/>
-                </NavItem>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>*/}
           <h1>HEADER</h1>
           {this.props.children}
     </div>
