459592021f562f58a3716ed63fe6e266d68e7a45
KE-NIA <kania.kenneth@web.de>
2017-02-04 18:29:14 +0100

    jepiiiiii the f***** work flow is working

--- src/components/MeasureCircle.js
+++ src/components/MeasureCircle.js
@@ -1,40 +1,32 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Button, Grid, Row, Col } from 'react-bootstrap'
 import CircleSvg from './CircleSvg';
+import { hashHistory } from 'react-router';
 
-const mapStateToProps = (state) => {
-    return{
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return{
-    };
-};
-
-@connect(mapStateToProps, mapDispatchToProps)
 export default class MeasureCircle extends React.Component {
 
   constructor(props) {
     super(props);
+    this.onMeasure = this.onMeasure.bind(this);
   }
 
-  componentDidMount(){
+  onMeasure(){
+    hashHistory.push('/circularityresult')
   }
 
   render() {
-    const dummySentences = ['Um den Kreis zu messen führen sie die Kugel bitte enlang des Kreises'];
+    const dummySentences = ['Take the reflector and move alongside the inner circle.If '];
 
     return (
-      <div style={{marginTop: '74px'}}>
-        <h2>Bitte führen Sie die Kugel entlang des Umrings</h2>
-        <CircleSvg showFlangePoints={false} />
+      <div style={{marginTop: '44px'}}>
+        <h2>Please move the reflector along the inner circle</h2>
+        <CircleSvg showFlangePoints={false} showInnerSmr={true} />
         <Grid>
           <Row className="show-grid" class="text-center">
             <Col sm={6} md={4} mdOffset={4}><br/>{dummySentences.slice(0, 2).join(' ')}</Col>
           </Row>
         </Grid>
-        <Button href="#/circularityresult">Messen</Button>
+        <Button class="btn btn-default central-button" onClick={this.onMeasure}>MEASURE</Button>
       </div>
     );
   }
